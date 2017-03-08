c65a316eff749a17c50dd395a8106718a63b1745
riegbaumhauer <riegbaumhauer@gmail.com>
2017-03-08 12:18:23 -0500

    init

---
+++ index.js
@@ -0,0 +1,76 @@
+
+//needed vars:
+
+var dots=0;
+var old_X=0;
+var old_Y=0;
+
+var change = function(e){
+    console.log("CAHBHEN");
+    this.setAttribute("fill", "red");
+    //e.stopPropagation();
+};
+
+var crtCirc = function(x,y){
+    var c = document.createElementNS("http://www.w3.org/2000/svg","circle");
+    c.setAttribute("cx", x);
+    c.setAttribute("cy", y);
+    c.setAttribute("r", "30");
+    c.setAttribute("fill", "green");
+
+    c.addEventListener("click", change);
+    
+    return c;
+};
+
+//fxn to draw the dots, and attach the lines
+var drop_dot = function(event){
+    var x = event.offsetX;
+    var y = event.offsetY;
+
+    var c = crtCirc(x,y);
+    
+    dots++;
+
+    console.log("dot");
+    //console.log("red"+e.target);
+    /*
+    var c = document.createElementNS("http://www.w3.org/2000/svg","circle");
+    c.setAttribute("cx", x);
+    c.setAttribute("cy", y);
+    c.setAttribute("r", "30");
+    c.setAttribute("fill", "green");
+     */
+
+    document.getElementById("vimage").appendChild(c);
+}
+
+
+
+
+//fxn to clear
+var clear_svg = function(){
+    dots=0;
+    old_X=0;
+    old_Y=0;
+    
+    //var c = document.createElementNS("http://www.w3.org/2000/svg","circle");
+
+    var svg = document.getElementById("vimage");
+    while (svg.lastChild) {
+	svg.removeChild(svg.lastChild);
+    };
+    started = false;
+    /*
+    c.setAttribute("cx", "250");
+    c.setAttribute("cy", "250");
+    c.setAttribute("r", "5000");
+    c.setAttribute("fill", "green");
+
+    document.getElementById("vimage").appendChild(c);
+*/
+}
+
+
+document.getElementById("vimage").addEventListener("click", drop_dot);
+document.getElementById("clear").addEventListener("click", clear_svg);
